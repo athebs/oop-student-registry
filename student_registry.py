71decd324116973e7bad3c9f7756b734f7ee6511
athebs <athena.s.tafur@gmail.com>
2024-02-07 10:02:28 -0600

    class review

--- student_registry.py
+++ student_registry.py
@@ -1,40 +1,44 @@
 class Student:
-    def __init__(self, name) -> None:
-        self._name = name 
-        self._age = 13 
-        self._grade = "12th" 
+    # adding attriubete to this class: name age and grade 
+    # the init method 
+    # passing arguments into the init method 
+
+    def __init__(self, name, age =13 , grade = "12th") -> None:
+        self.name = name 
+        self.age = age 
+        self.grade = grade 
     
     
     @property 
     def get_name(self):
-        return self._name
+        return self.name
     
     @get_name.setter
     def set_name(self, new_name):
-        #  Updates the students name only if the student name
-        #  <br/> is 3 characters or more, holds 
-        # no spaces or special characters,<br/> 
-        # and is in title format 
-        pass
+        if len(new_name)<= 3: 
+            raise "Name must be longer than three Characters!"
+        if new_name.isalpha() and " " not in new_name: 
+            self.name = new_name
+       
 
     @property 
     def get_age(self):
-        return self._age
+        return self.age
     
     @get_age.setter
     def set_age(self, new_age):
-        # updates the students age only if the age value
-        # is an int, is greater than 11 and lower 
-        # than 18 
-
         return "hi" 
     
     @property 
     def get_grade(self):
+        if self.get_grade == None: 
+            return "No Grade"
         return self.grade
-    
+    # syntax would be @<GETTER-NAME>.setter 
     @get_grade.setter
     def set_grade(self, new_grade):
+        self.grade = new_grade 
+
         #  Updates a students grade only if the grade
         #  falls within <br/> 9th - 12th grade
         # and the value is formatted with "th" <br/>next
@@ -46,7 +50,21 @@ class Student:
         return f"Student 1:{self._name}"
 
 
-Bob = Student("Bob")
-print(Bob)
+bob = Student("Bob", 18, "12th")
+alice =Student("alice", 11, "2nd")
+#direct access the name 
+print(alice.name)
+# indrictest access the name 
+print(alice.get_name)
+
+#gets the get value with the get function 
+print(alice.get_age) 
+
+# when using a setter you must use the "=" because you can not call 
+#  the setter cant use it like a function 
+# like so 
+alice.set_grade = 10 
+# not like this alice.set_grade(10 )
+print(alice.get_grade)
 
        
